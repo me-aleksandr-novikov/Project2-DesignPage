aa15c166725db6924d130e087bd9deef9e2c1d3f
Aleksandr Novikov <65459275+novikov-main-acc@users.noreply.github.com>
2022-03-24 00:40:10 +1000

    PictureOnMouseOver

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
+import pictureSize from './modules/pictureSize';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -22,4 +23,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
+    pictureSize('.sizes-block');
+
 });
