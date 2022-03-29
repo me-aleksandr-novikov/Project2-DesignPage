859b895b03483be0ec4369c04a5b326fabddc4f6
Aleksandr Novikov <65459275+novikov-main-acc@users.noreply.github.com>
2022-03-29 23:27:39 +1000

    scrollinStartLesson

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,9 @@ import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
 import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
+import burger from './modules/burger';
+import scrolling from './modules/scrolling';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -24,5 +27,8 @@ window.addEventListener('DOMContentLoaded', () => {
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
     pictureSize('.sizes-block');
+    accordion('.accordion-heading');
+    burger('.burger-menu', '.burger');
+    scrolling();
 
 });
